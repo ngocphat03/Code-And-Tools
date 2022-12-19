c7106d7fe3e0f47e503e61d07a3b1bf5192aaa5c
ngocphat03 <107571396+ngocphat03@users.noreply.github.com>
2022-12-19 08:45:56 +0700

    Create Json_File_Save_And_Load_Template.cs

---
+++ Json_File_Save_And_Load_Template.cs
@@ -0,0 +1,79 @@
+using System.IO;
+using UnityEngine;
+using Newtonsoft.Json;
+using System.Collections;
+using Newtonsoft.Json.Converters;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class JsonFileSaveAndLoadTemplate : MonoBehaviour
+{
+    ListModel _listModel = new();T
+    public void Test()
+    {
+        this.LoadDataFromFile();
+        _listModel.PlayerModel.Add(new Model
+        {
+            id = Random.Range(1, 100),
+            name = "AXit",
+            age = 18,
+            sex = "Male"
+        });
+
+        this.SaveDataToFile();
+    }
+    public void SaveDataToFile()
+    {
+        try
+        {
+            //Save data to directory
+            var serializer = new JsonSerializer();
+            using var sw = new StreamWriter(Application.persistentDataPath + "/dataUser.json");
+            using JsonWriter writer = new JsonTextWriter(sw);
+            serializer.Serialize(writer, _listModel);
+        }
+        catch 
+        { 
+            //Create empty data when there is no file
+            this.CreateEmptyFile(); 
+        }
+    }
+
+    public void LoadDataFromFile()
+    {
+        try
+        {
+            //Get data from directory and assign it to model
+            var outputJson = File.ReadAllText(Application.persistentDataPath + "/dataUser.json");
+            var loadedUserData = JsonConvert.DeserializeObject<ListModel>(outputJson);
+            _listModel.PlayerModel = loadedUserData.PlayerModel;
+        }
+        catch
+        {
+            //Create empty data when there is no file
+            this.CreateEmptyFile();
+        }
+    }
+
+    public void CreateEmptyFile()
+    {
+        ListModel __listModel = new();
+        var serializer = new JsonSerializer();
+        using var sw = new StreamWriter(Application.persistentDataPath + "/dataUser.json");
+        using JsonWriter writer = new JsonTextWriter(sw);
+        serializer.Serialize(writer, __listModel);
+    }
+}
+
+public class Model
+{
+    public int id;
+    public string name;
+    public int age;
+    public string sex;
+}
+
+public class ListModel
+{
+    public List<Model> PlayerModel = new List<Model>();
+}
